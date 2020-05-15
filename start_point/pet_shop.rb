7bc0cadd4c8b43e0168f0490007efeebea05ec81
Rumen Roger <rumenroger@gmail.com>
2020-05-15 23:46:46 +0100

    #01. Get shop name

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -1,7 +1,50 @@
 # 1. Get shop name
+# access array one level within main array
+
+def pet_shop_name(shop)
+    return shop[:name]
+end
 
 # 2. Get total cash
 
 # 3. Add or remove cash
 
 # 4. Get pets sold
+
+# 5. Increase pets sold
+
+# 6. Stock count
+
+# 7. Sort all pets by breed [found]
+
+# 8. Sort all pets by bread [no_found]
+
+# 9. Find pet by name
+
+# 10. Find pet by name [nil]
+
+# 11. Remove pet by name
+
+# 12. Add pet to stock
+
+# 13. Customer cash
+
+# 15. Remove customer cash
+
+# 16. Customer pet count
+
+# 17. Add pet to customer
+
+# --- OPTIONAL ---
+
+# 18. Customer can afford pet - sufficient funds [true]
+
+# 19. Customer can afford pet - insufficient funds [false]
+
+# 20. Customer can afford pet - exact funds [true]
+
+# 21. Sell pet to customer - pet found
+
+# 22. Sell pet to customer - pet not found
+
+# 23. Sell pet to customer - insufficient funds
