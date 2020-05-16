aa6058d56b7b5861eff88b892073946f99181c85
Rumen Roger <rumenroger@gmail.com>
2020-05-16 17:19:23 +0100

    #22. Sell pet to customer - pet not found

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -175,39 +175,12 @@ def sell_pet_to_customer(pets, pet, customer)
 # 3. reduce customer cash
 # 4. increase shop cash
 
-    # if pet ==! nil
-
+    if (pet == nil) || (customer[:cash] < pet[:price])
+        return nil
+    else
         pet_bought = customer[:pets].push(pet)
         pets[:admin][:pets_sold] += pet_bought.count
         customer[:cash] -= pet[:price]
         pets[:admin][:total_cash] += pet[:price]
-    # else
-    #     return nil
-    # end
-
+    end
 end
-
-
-#   def test_sell_pet_to_customer__pet_not_found
-#     customer = @customers[0]
-#     pet = find_pet_by_name(@pet_shop,"Dave")
-
-#     sell_pet_to_customer(@pet_shop, pet, customer)
-
-#     assert_equal(0, customer_pet_count(customer))
-#     assert_equal(0, pets_sold(@pet_shop))
-#     assert_equal(1000, customer_cash(customer))
-#     assert_equal(1000, total_cash(@pet_shop))
-#   end
-
-  # def test_sell_pet_to_customer__insufficient_funds
-  #   customer = @customers[1]
-  #   pet = find_pet_by_name(@pet_shop,"Arthur")
-
-  #   sell_pet_to_customer(@pet_shop, pet, customer)
-
-  #   assert_equal(0, customer_pet_count(customer))
-  #   assert_equal(0, pets_sold(@pet_shop))
-  #   assert_equal(50, customer_cash(customer))
-  #   assert_equal(1000, total_cash(@pet_shop))
-  # end
